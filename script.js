86bf3a15564a87287b34d83433bdac788d0e1883
gaby-lorraynne <gabyalmeidacom@hotmail.com>
2024-04-01 11:12:11 -0400

    Projeto simples para testar cores ou bordas

---
+++ script.js
@@ -0,0 +1,8 @@
+let content_box = document.getElementById('content-box');
+let input = document.querySelector('input');
+
+input.addEventListener('input', () => {
+    let value = input.value;
+    content_box.style.borderRadius = value;
+    content_box.style.background = value;
+});
